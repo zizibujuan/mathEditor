bdd328b7ad82a52edd590e7704df17c7a5a7fb2a
jzw <zhengwei.jin@gmail.com>
2013-11-20 21:49:17 +0800

    添加travis-ci脚本

--- static/mathEditor/tests/intern.js
+++ static/mathEditor/tests/intern.js
@@ -15,32 +15,27 @@ define({
 	// Note that the `build` capability will be filled in with the current commit ID from the Travis CI environment
 	// automatically
 	capabilities: {
-		'selenium-version': '2.35.0'
+		'selenium-version': '2.37.0'
 	},
 
 	// Browsers to run integration testing against. Note that version numbers must be strings if used with Sauce
 	// OnDemand. Options that will be permutated are browserName, version, platform, and platformVersion; any other
 	// capabilities options specified for an environment will be copied as-is
 	environments: [
-		/*{ browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
-		{ browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
-		{ browserName: 'firefox', version: '22', platform: [ 'Linux', 'Windows 7' ] },
-		{ browserName: 'firefox', version: '21', platform: 'Mac 10.6' },
-		{ browserName: 'chrome', platform: [ 'Linux', 'Mac 10.8', 'Windows 7' ] },
-		{ browserName: 'safari', version: '6', platform: 'Mac 10.8' }
-		{ browserName: 'chrome', platform: [ 'LINUX'] }*/
-		/*
-		{ browserName: 'chrome'},
-		{ browserName: 'firefox'}
-		*/
-		{ browserName: 'internet explorer'}
+        { browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
+        { browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
+        { browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
+        { browserName: 'firefox', version: '25', platform: [ 'OS X 10.6', 'Windows 7' ] },
+        { browserName: 'firefox', version: '24', platform: 'Linux' },
+        { browserName: 'chrome', version: '', platform: [ 'Linux', 'OS X 10.6', 'Windows 7' ] },
+        { browserName: 'safari', version: '6', platform: 'OS X 10.8' }
 	],
 
 	// Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
 	maxConcurrency: 3,
 
 	// Whether or not to start Sauce Connect before running tests
-	useSauceConnect: false,
+	useSauceConnect: true,
 
 	// Connection information for the remote WebDriver service. If using Sauce Labs, keep your username and password
 	// in the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables unless you are sure you will NEVER be
@@ -49,27 +44,25 @@ define({
 		host: 'localhost',
 		port: 4444
 	},
+	
+    // The desired AMD loader to use when running unit tests (client.html/client.js). Omit to use the default Dojo
+    // loader
+    useLoader: {
+            'host-node': 'dojo/dojo',
+            'host-browser': 'node_modules/dojo/dojo.js'
+    },
 
 	// Configuration options for the module loader; any AMD configuration options supported by the Dojo loader can be
 	// used here
 	loader: {
 		// Packages that should be registered with the loader in each testing environment
 		baseUrl: "",
-		packages: [ { name: 'dojo', location: 'dojo/' },
-		            { name: 'mathEditor', location: 'mathEditor/' }
-		            ]
+		packages: [ 
+		    { name: 'dojo', location: 'dojo/' },        
+		    { name: 'dijit', location: 'dijit/' },
+		    { name: 'mathEditor', location: 'mathEditor/' }
+		]
 	},
-//
-//	{ baseUrl: internDir + '../../',
-//		  packages: [
-//		    { name: 'intern',
-//		      location: internDir }
-//		  ],
-//		  map: { intern: {
-//		    dojo: 'intern/node_modules/dojo',
-//		    chai: 'intern/node_modules/chai/chai'
-//		  } }
-//		}
 	
 	// Non-functional test suite(s) to run in each browser
 	// /* 'myPackage/tests/foo', 'myPackage/tests/bar' */
